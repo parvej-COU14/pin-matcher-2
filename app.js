a0f4476382fd569b8c4394ac54ead59f652c55ac
parvej mosharaf <parvejmosharaf62462@gmail.com>
2021-07-01 17:49:09 +0600

    done pin generator

---
+++ app.js
@@ -0,0 +1,17 @@
+//  function getPin(){
+//           const random=Math.random()*10000;
+//           const pin =(random + '').split('.')[0];
+//           if(pin.length===4){
+//               return pin;
+//           }
+//           else{
+//               console.log('shorter to pin', pin);
+//               return getPin();
+//           }
+//       }
+      
+//       const generateButton=document.getElementById("generateBtn");
+//       generateButton.addEventListener('click',function(){
+//           const pinBtn=document.getElementById('pin');
+//           pinBtn.value=getPin();
+//       })
